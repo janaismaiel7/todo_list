c31dfec7d5fd8c89d70953378de0896307c4b64f
janaismaiel7 <janna.aismaiel@gmail.com>
2024-08-20 16:30:02 +0300

    todo init

--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,9 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:todo_list/firebase_options.dart';
 import 'package:todo_list/home/homeScreen.dart';
-import 'package:todo_list/login/loginScreen.dart';
 import 'package:todo_list/mythemedata.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'package:todo_list/provider/authUserProvider.dart';
-import 'package:todo_list/provider/listProvider.dart';
-import 'package:todo_list/register/registerScreen.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
- await FirebaseFirestore.instance.enableNetwork();
-  runApp(MultiProvider(
-providers: [
-  ChangeNotifierProvider(create: (context)=>Listprovider()),
-  ChangeNotifierProvider(create: (context)=>Authuserprovider()),
-
-],
-child: MyApp(),
-
-
-
-
-  )
-    
-  );
+void main() {
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -39,12 +13,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        initialRoute: Loginscreen.routeName,
-        routes: {Homescreen.routeName: (context) => Homescreen(),
-        Registerscreen.routeName: (context) => Registerscreen(),
-                Loginscreen.routeName: (context) => Loginscreen()
+      debugShowCheckedModeBanner: false,
+      initialRoute: Homescreen.routeName,
+      routes: {
+        Homescreen.routeName:(context)=> Homescreen()
         },
-        theme: Mythemedata.lightTheme);
+        theme: Mythemedata. lightTheme
+       
+    );
   }
 }
